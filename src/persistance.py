42d8b783af18e9e4b451ead90b00777697e5157e
hbeun8 <hbeun8@outlook.com>
2025-07-25 12:54:04 +0100

    adopted Johns persistance

--- src/persistance.py
+++ src/persistance.py
@@ -4,30 +4,36 @@ We have to options for persistance:
     2. append-only file.
 '''
 
-class AppendontyPersister: 3 usages
-def
-_init__(self, filename):
-self._fiLename = filename
-self._file = open(filename,
-1ode= aD, DuTterng=0
-def log_command(self, command): 8 usages (8 dynamic) & John Crickett self._file write (f"*fLen(command) Hr\n". encode())
-for item in command:
-selt._file.wr1te(1tem.tlle_encodeo)
-def restore_from_file(filename, datastore): 2 usages 1 John Crickett
-buffer = bytearray)
-with open(filename, "rb") as f:
-while True:
-data = f.read(4096)
-if not data: break
-buffer.extend (data)
-while True:
-frame, frame_size = extract_frame_from_buffer(buffer)
-if frame:
-buffer = buffer[frame_size:]
-result = handLe_command (frame, datastore)
-if isinstance (result, Error):
-print("Error corrupt AOF file")
-return
+class AppendOnlyPersister:
+    def __init__(self, filename):
+        self._filename = filename
+        self._file = open(filename, mode='ab', buffering=0)
+
+    def log_command(self, command):
+        # Write the length of the command followed by the encoded command items
+        self._file.write(f"{len(command)}\n".encode())
+        for item in command:
+            self._file.write(item.encode())
+
+def restore_from_file(filename, datastore):
+    buffer = bytearray()
+    with open(filename, "rb") as f:
+        while True:
+            data = f.read(4096)
+            if not data:
+                break
+            buffer.extend(data)
+
+    while True:
+        frame, frame_size = extract_frame_from_buffer(buffer)
+        if not frame:
+            break
+        buffer = buffer[frame_size:]
+        result = handle_command(frame, datastore)
+        if isinstance(result, Error):
+            print("Error: corrupt AOF file")
+            return
+
 
 
 def stop_expiry_monitor():
